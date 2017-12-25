234b5f567aa5cc056859100b1ee8a3bca449c432
lrrr <350552948@qq.com>
2017-12-25 18:58:02 +0800

    界面test

---
+++ LibraryinfoSystem/MainWin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryinfoSystem
+{
+    public partial class MainWin : Form
+    {
+        public MainWin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BorrowWin bw = new BorrowWin();
+            bw.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
