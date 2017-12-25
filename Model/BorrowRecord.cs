eeb55806ee2dd8a6c79c71429de4265ed973f994
LiZeC <tomgreem123@gmail.com>
2017-12-25 08:39:44 +0800

    添加数据库基本函数和部分数据库实体

---
+++ Model/BorrowRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class BorrowRecord
+    {
+        private int _index;
+        public int Index
+        {
+            get
+            {
+                return _index;
+            }
+        }
+
+        private int _cardID;
+        public int CardID
+        {
+            get
+            {
+                return _cardID;
+            }
+        }
+
+
+    }
+}
