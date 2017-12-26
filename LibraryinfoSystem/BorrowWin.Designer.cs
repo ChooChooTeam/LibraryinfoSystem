aa0c39208b44a33769ac91bfd7fd4883f84117eb
marykt <wolianlxw@sina.com>
2017-12-26 23:13:19 +0800

    fix

--- LibraryinfoSystem/BorrowWin.Designer.cs
+++ LibraryinfoSystem/BorrowWin.Designer.cs
@@ -83,7 +83,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(90, 30);
             this.textBox1.TabIndex = 1;
-            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            //this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // button1
             // 
@@ -302,7 +302,7 @@
             this.textBox9.Size = new System.Drawing.Size(90, 28);
             this.textBox9.TabIndex = 25;
             this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged_1);
-            this.textBox9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox9_KeyPress);
+           // this.textBox9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox9_KeyPress);
             // 
             // label12
             // 
