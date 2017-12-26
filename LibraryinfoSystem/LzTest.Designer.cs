2668cfa7e017de9980644405f0c4a597160941a5
LiZeC <tomgreem123@gmail.com>
2017-12-26 16:42:28 +0800

    修改数据库模式

--- LibraryinfoSystem/LzTest.Designer.cs
+++ LibraryinfoSystem/LzTest.Designer.cs
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvBook = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.isbn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bookName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,11 +43,12 @@
             this.dgvBook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvBook.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.isbn,
-            this.bookName});
+            this.bookName,
+            this.bookNum});
             this.dgvBook.Location = new System.Drawing.Point(66, 75);
             this.dgvBook.Name = "dgvBook";
             this.dgvBook.RowTemplate.Height = 23;
-            this.dgvBook.Size = new System.Drawing.Size(242, 150);
+            this.dgvBook.Size = new System.Drawing.Size(341, 150);
             this.dgvBook.TabIndex = 0;
             this.dgvBook.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBook_CellContentClick);
             // 
@@ -61,6 +65,9 @@
             // isbn
             // 
             this.isbn.DataPropertyName = "isbn";
+            dataGridViewCellStyle1.Format = "D15";
+            dataGridViewCellStyle1.NullValue = null;
+            this.isbn.DefaultCellStyle = dataGridViewCellStyle1;
             this.isbn.HeaderText = "ISBN";
             this.isbn.Name = "isbn";
             this.isbn.ReadOnly = true;
@@ -72,11 +79,20 @@
             this.bookName.Name = "bookName";
             this.bookName.ReadOnly = true;
             // 
+            // bookNum
+            // 
+            this.bookNum.DataPropertyName = "bookNum";
+            dataGridViewCellStyle2.Format = "D6";
+            dataGridViewCellStyle2.NullValue = null;
+            this.bookNum.DefaultCellStyle = dataGridViewCellStyle2;
+            this.bookNum.HeaderText = "书籍数量";
+            this.bookNum.Name = "bookNum";
+            // 
             // LzTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 261);
+            this.ClientSize = new System.Drawing.Size(485, 261);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dgvBook);
             this.Name = "LzTest";
@@ -93,5 +109,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridViewTextBoxColumn isbn;
         private System.Windows.Forms.DataGridViewTextBoxColumn bookName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookNum;
     }
 }
