d81c87f5e4d38c383af77bc498d3aabc1064d627
huiqingli <1259701203@qq.com>
2017-12-26 20:53:09 +0800

    通过索书号查找书名和出版社

--- LibraryinfoSystem/LzTest.Designer.cs
+++ LibraryinfoSystem/LzTest.Designer.cs
@@ -31,10 +31,11 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvBook = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
             this.isbn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bookName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.bookNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBook)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,16 +52,6 @@
             this.dgvBook.Size = new System.Drawing.Size(341, 150);
             this.dgvBook.TabIndex = 0;
             this.dgvBook.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBook_CellContentClick);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(190, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(46, 20);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // isbn
             // 
@@ -88,11 +79,32 @@
             this.bookNum.HeaderText = "书籍数量";
             this.bookNum.Name = "bookNum";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // LzTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(485, 261);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dgvBook);
             this.Name = "LzTest";
@@ -110,5 +122,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn isbn;
         private System.Windows.Forms.DataGridViewTextBoxColumn bookName;
         private System.Windows.Forms.DataGridViewTextBoxColumn bookNum;
+        private System.Windows.Forms.Button button2;
     }
 }
