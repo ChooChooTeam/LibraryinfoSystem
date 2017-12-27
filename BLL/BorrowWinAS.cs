f49362ff14ef61f24d197fee1db1cb5b5e90433b
marykt <wolianlxw@sina.com>
2017-12-27 20:51:57 +0800

    fix:some bug

--- BLL/BorrowWinAS.cs
+++ BLL/BorrowWinAS.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Model;
 using DAL;
+using System.Data.SqlClient;
+using Utility;
+
 namespace BLL
 {
     public class BorrowWinAS
@@ -19,6 +22,23 @@ namespace BLL
                 LibraryCard card = LibraryCardIfo.QueryLibraryCardIfo(libraryCardID);
                 return card;
  
+
+        }
+        public static bool GetUserHaveMoneyToPay(String libraryCardID) {
+            DataTable dataTable;
+            string sql = "select damageRecord.damageIndex,damageReason.damageExplain,circuBookClass.bookName,damageRecord.damageTime,damageRecord.damageMoney " +
+                   "from damageRecord join(circuBook join circuBookClass on circuBook.isbn = circuBookClass.isbn)on damageRecord.circuBookNo = circuBook.circuBookNo " +
+                   "join damageReason on damageReason.damageReasonIndex = damageRecord.damageReasonIndex " +
+                   "where damageRecord.damageRtnTIme is NULL and damageRecord.libraryCardID = @libraryCardID";
+            SqlParameter para = new SqlParameter("@libraryCardID", libraryCardID);
+
+            dataTable = SQLHelper.getDataTable(sql, para);
+            //dataTable.
+            if (dataTable.Rows.Count == 0)
+            {
+                return false;
+            }
+            return true;
 
         }
        
