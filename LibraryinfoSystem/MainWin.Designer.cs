48611f0c19bc94b680f66f8791018c8935e52a7f
LiZeC <tomgreem123@gmail.com>
2018-01-03 08:58:28 +0800

    删除不需要的界面

--- LibraryinfoSystem/MainWin.Designer.cs
+++ LibraryinfoSystem/MainWin.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.bthExample = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(134, 71);
+            this.button1.Location = new System.Drawing.Point(100, 57);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 38);
+            this.button1.Size = new System.Drawing.Size(66, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "借书";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,29 +48,21 @@
             // bthExample
             // 
             this.bthExample.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bthExample.Location = new System.Drawing.Point(134, 125);
+            this.bthExample.Location = new System.Drawing.Point(100, 100);
+            this.bthExample.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bthExample.Name = "bthExample";
-            this.bthExample.Size = new System.Drawing.Size(88, 38);
+            this.bthExample.Size = new System.Drawing.Size(66, 30);
             this.bthExample.TabIndex = 1;
             this.bthExample.Text = "还书";
             this.bthExample.UseVisualStyleBackColor = true;
             this.bthExample.Click += new System.EventHandler(this.bthExample_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(137, 236);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "db_test";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(134, 178);
+            this.button2.Location = new System.Drawing.Point(100, 142);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(88, 40);
+            this.button2.Size = new System.Drawing.Size(66, 32);
             this.button2.TabIndex = 3;
             this.button2.Text = "缴费";
             this.button2.UseVisualStyleBackColor = true;
@@ -78,9 +70,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(268, 269);
+            this.button4.Location = new System.Drawing.Point(201, 215);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 33);
+            this.button4.Size = new System.Drawing.Size(56, 26);
             this.button4.TabIndex = 4;
             this.button4.Text = "退出";
             this.button4.UseVisualStyleBackColor = true;
@@ -88,15 +81,15 @@
             // 
             // MainWin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(341, 299);
+            this.ClientSize = new System.Drawing.Size(256, 239);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.bthExample);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainWin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "借还书服务系统";
@@ -108,7 +101,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button bthExample;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button4;
     }
